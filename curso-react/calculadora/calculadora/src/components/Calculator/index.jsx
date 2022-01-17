7ab4f98b7f99dd8e56f864ba1e5ea5b059d4535f
wendelsilva <wendel245.ws@gmail.com>
2022-01-17 08:50:50 -0300

    feat: init calculator project

---
+++ curso-react/calculadora/calculadora/src/components/Calculator/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './styles.css';
+
+export function Calculator() {
+  return (
+    <div className="calculator">
+      <h1>Calculadora</h1>
+    </div>
+  );
+}
