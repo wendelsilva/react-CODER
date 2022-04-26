4dfcf940628ca5f452b20db5ae3973b9530f449e
wendelsilva <wendel245.ws@gmail.com>
2022-04-26 16:07:34 -0300

    feat: nextjs fundamentals

---
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Fundamentos de Next.js e React.js</h1>
+            <h2>Vamos estudar esse Framework</h2>
+        </div>
+    )
+}
---
+++ aulao_nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,7 @@
+export default function Jsx() {
+    return (
+        <div>
+            <h1>Jsx é um Conceito Central</h1>
+        </div>
+    )
+}
