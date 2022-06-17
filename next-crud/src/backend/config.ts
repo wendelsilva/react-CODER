fa9b504497424b80367f86289404ee1ae5931c1c
wendelsilva <wendel245.ws@gmail.com>
2022-06-17 14:59:40 -0300

    feat: modify firebase version

--- next-crud/src/backend/config.ts
+++ next-crud/src/backend/config.ts
@@ -1,5 +1,5 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 if(!firebase.apps.length) {
     firebase.initializeApp({
