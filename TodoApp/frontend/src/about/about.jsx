bf9872c924db5e5c59a2f6e9864b44527f2c26bb
wendelsilva <wendel245.ws@gmail.com>
2022-08-18 19:53:12 -0300

    todo app frontend

---
+++ TodoApp/frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure provident natus sunt, dolore reiciendis quidem itaque error vel earum non placeat nihil quis ea, voluptate cum dicta in incidunt reprehenderit.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium quidem adipisci distinctio officiis dolore reprehenderit, vero architecto eius est cupiditate facilis exercitationem ullam aliquid, laborum suscipit hic at velit! Necessitatibus.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequuntur obcaecati ipsam neque omnis culpa animi assumenda autem ab. Pariatur possimus non iure sed tempora eveniet quod quidem dolores. Corporis!</p>
+    </div>
+)
---
+++ TodoApp/frontend/src/todo/todo.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PageHeader from '../template/pageHeader';
+import TodoForm from './todoForm';
+import TodoList from './todoList';
+
+export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+
+                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoList />
+            </div>
+        )
+    }
+}
